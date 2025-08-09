0c6bd8f91151907dcdb892c6e581359bc3b5d0a7
Yeicor <4929005+Yeicor@users.noreply.github.com>
2025-08-09 20:14:25 +0200

    Fix CI

--- vcpkg_triplets/wasm32-emscripten-pic.cmake
+++ vcpkg_triplets/wasm32-emscripten-pic.cmake
@@ -26,5 +26,5 @@ set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${EMSCRIPTEN_ROOT}/cmake/Modules/Platform/Em
 # CUSTOM:  (hopefully all dependencies use cmake...)
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS}
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON  # Force POSITION_INDEPENDENT_CODE to ON
-    -DCMAKE_SHARED_LINKER_FLAGS=-allow-multiple-definition  # Allow multiple definitions in the shared linker flags
+    -DCMAKE_SHARED_LINKER_FLAGS=-Wl,-allow-multiple-definition  # Allow multiple definitions in the shared linker flags
 )
