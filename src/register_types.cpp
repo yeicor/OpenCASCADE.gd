668461de30aeae498998c294b7baa484454848d1
Yeicor <4929005+Yeicor@users.noreply.github.com>
2025-08-17 20:04:58 +0200

    Remove Python binding generator and update registration
    - Delete godot_cpp_binding_generator directory and its files - Add
    gen_bindings.sh script for regenerating bindings - Update registration
    to use new ocgd_library_init function - Update
    step_importer/register_types.h includes and formatting

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,14 +1,20 @@
 #include "step_importer/register_types.h"
-#include "gdextension_interface.h"
-#include "gen_bindings/ocgd_register.h"
+#ifdef OCGD_AUTO_BINDINGS_ENABLED
+#include "gen_bindings/register_types.h"
+#endif
+
 extern "C" {
-// Initialization
-GDExtensionBool GDE_EXPORT opencascade_gd_library_init(
-    GDExtensionInterfaceGetProcAddress p_get_proc_address,
-    GDExtensionClassLibraryPtr p_library,
-    GDExtensionInitialization *r_initialization) {
-  GDExtensionBool r = opencascade_gd_library_init_step_importer(p_get_proc_address, p_library, r_initialization);
-  if (!r) return r;
-  return ocgd_gd_library_init_bindings(p_get_proc_address, p_library, r_initialization);
-}
+    // Initialization
+    GDExtensionBool GDE_EXPORT opencascade_gd_library_init(
+        GDExtensionInterfaceGetProcAddress p_get_proc_address,
+        GDExtensionClassLibraryPtr p_library,
+        GDExtensionInitialization *r_initialization) {
+        GDExtensionBool r = opencascade_gd_library_init_step_importer(p_get_proc_address, p_library, r_initialization);
+        if (!r) return r;
+#ifdef OCGD_AUTO_BINDINGS_ENABLED
+        return ocgd_library_init(p_get_proc_address, p_library, r_initialization);
+#else
+        return true;
+#endif
+    }
 }
--- src/step_importer/register_types.h
+++ src/step_importer/register_types.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#include <godot_cpp/classes/editor_import_plugin.hpp>
+#include <gdextension_interface.h>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+using namespace godot;
 
 GDExtensionBool opencascade_gd_library_init_step_importer(GDExtensionInterfaceGetProcAddress p_get_proc_address,
-                                                          GDExtensionClassLibraryPtr p_library,
-                                                          GDExtensionInitialization* r_initialization);
+                                                       GDExtensionClassLibraryPtr p_library,
+                                                       GDExtensionInitialization *r_initialization);
